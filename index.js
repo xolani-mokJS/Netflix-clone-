9e4e98f19742741e2b7b06c01decc0ecefc1e102
xolani-mokJS <70220609+xolani-mokJS@users.noreply.github.com>
2022-09-23 22:43:03 +0200

    hero section

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const form = document.getElementById('login-form');
 const signUpForm = document.getElementById('sign-up-form');
 
-form.addEventListener('submit', function(f){
+form.addEventListener('submit', (f)=>{
     f.preventDefault();
     window.location.href = '\pages\login.html';
 });
     
-signUpForm.addEventListener('submit', function(e){
+signUpForm.addEventListener('submit', (e)=>{
    e.preventDefault();
     
     const username = document.getElementById('login-email').value;
