7926acf8920323fe47ec4988d05e00fb1b16c8fc
xolani-mokJS <70220609+xolani-mokJS@users.noreply.github.com>
2022-09-29 16:39:18 +0200

    file changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import components
-import './Js Components/movie-list.js';
-import './Js Components/movie-preview.js';
-import './Js Components/app.js';
+import './Components/movie-list.js';
+import './Components/movie-preview.js';
+import './Components/app.js';
 
 const form = document.getElementById('login-form');
 const signUpForm = document.getElementById('sign-up-form');
