1e19e12edd25a6c7c5ae979ca15157e17c7df99f
xolani-mokJS <70220609+xolani-mokJS@users.noreply.github.com>
2022-10-03 10:00:08 +0200

    video

--- Components/movie-preview.js
+++ Components/movie-preview.js
@@ -109,7 +109,7 @@ class MoviePreview extends LitElement {
         <div class="movie-preview">
             <div class="resting show-cover" style="${styleMap(backgroundStyle)}">
                 <div class="preview hidden-items">
-                    <img class="image" src="${this.image}" alt="${this.label}">
+                    <video src="/Images/trailer.mp4" loop></video>
                     <p class="movie-name">${this.label}</p>
                     <div class="hidden-buttons">
                         <button class="buttons-hidden">play</button>
