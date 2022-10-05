681abff3d358cdee040b15f457707c16a1872d3c
xolani-mokJS <70220609+xolani-mokJS@users.noreply.github.com>
2022-10-05 15:08:14 +0200

    refactor

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This is my take on the most famous movie-tv series streaming service in the world, Netflix.
-I've done this project as part of the Codespace academy curriculum leaning about APIs, Web components, and LIT.dev, building an almost fully functional web application.
+I've done this project as part of the Codespace academy curriculum learning about APIs, Web components, and LIT.dev, building an almost fully functional web application.
 
 Languages utilized:
 Html,
--- index.js
+++ index.js
@@ -2,3 +2,33 @@ import './components/movie-list.js'
 import './components/movie-preview.js'
 import './components/netflix-app.js'
 
+const loginForm = document.getElementById('login-form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const userEmail = document.getElementById('login-email');
+    const userPassword = document.getElementById('login-password');
+
+    if (userEmail.value === '' || userPassword.value === '')
+        {
+            document.getElementById('error').innerHTML = 'Please enter your email address and password';
+        }
+
+    else{
+        document.getElementById('error').innerHTML = '';
+        window.location.href = "/pages/home.html";
+
+    }
+
+    const userInfor = {
+        userName : userEmail.value, 
+        password: userPassword.value, 
+    };
+        
+
+   const myUser = localStorage.setItem('userInformation', JSON.stringify(userInfor));
+
+   console.log(myUser);
+
+});
